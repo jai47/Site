a99f38c9e8f5b36b780fdb1fd84c6518bd37672c
Jai <official.jaimishra@gmail.com>
2023-01-05 21:51:42 +0530

    using switch statement

--- Calculator/cal-script.js
+++ Calculator/cal-script.js
@@ -4,93 +4,82 @@ function display(num) {
     string.value += num;
 }
 
-function cal(){
-    try{
-        if(string.value.includes("%")){
-            let ans = Number(string.value.slice(0,string.value.indexOf("%"))) * Number(string.value.slice(string.value.indexOf("%")+1)) /100 ;
+function cal() {
+    try {
+        if (string.value.includes("%")) {
+            let ans = Number(string.value.slice(0, string.value.indexOf("%"))) * Number(string.value.slice(string.value.indexOf("%") + 1)) / 100;
             string.value = ans;
         }
         string.value = eval(string.value);
     }
-    catch(err) {
+    catch (err) {
         alert("Invalid request");
     }
 }
 
-function Cls(){
+function Cls() {
     string.value = "";
 }
 
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 16)
-        Cls();
-})
-
-function Del(){
-    string.value = string.value.slice(0,-1);
+function Del() {
+    string.value = string.value.slice(0, -1);
 }
 
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 13)
-        cal();
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 8)
-        Del();
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 96)
-        display(0);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 97)
-        display(1);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 98)
-        display(2);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 99)
-        display(3);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 100)
-        display(4);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 101)
-        display(5);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 102)
-        display(6);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 103)
-        display(7);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 104)
-        display(8);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 105)
-        display(9);
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 106)
-        display("*");
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 107)
-        display("+");
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 109)
-        display("-");
-})
-body.addEventListener('keyup', (e) =>{
-    if(e.keyCode === 111)
-        display("/");
+body.addEventListener('keyup', (e) => {
+    let y = e.keyCode;
+    switch (y) {
+        case 8:
+            Del();
+            break;
+        case 13:
+            cal();
+            break;
+        case 16:
+            Cls();
+            break;
+        case 96:
+            display(0);
+            break;
+        case 97:
+            display(1);
+            break;
+        case 98:
+            display(2);
+            break;
+        case 99:
+            display(3);
+            break;
+        case 100:
+            display(4);
+            break;
+        case 101:
+            display(5);
+            break;
+        case 102:
+            display(6);
+            break;
+        case 103:
+            display(7);
+            break;
+        case 104:
+            display(8);
+            break;
+        case 105:
+            display(9);
+            break;
+        case 106:
+            display("*");
+            break;
+        case 107:
+            display("+");
+            break;
+        case 109:
+            display("-");
+            break;
+        case 111:
+            display("/");
+            break;
+        default:
+            alert("Invalid Keystroke");
+    }
 })
